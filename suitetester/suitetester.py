f2ffc266ecff885760010c1b83838a6fac9106c7
Mark Rothlisberger <rothlmar@gmail.com>
2015-04-11 06:59:43 -1000

    cleans things up just a little

--- suitetester/suitetester.py
+++ suitetester/suitetester.py
@@ -14,8 +14,8 @@ def index():
 @app.route('/suites')
 def vals():
     opts = request.args.getlist('options')
-    optvals = [options.get(_) for _ in opts if _ in options]
-    if len(opts) != len(optvals):
+    optvals = [_ for _ in opts if _ in options.values()]
+    if len(optvals) == 0 or len(opts) != len(optvals):
         return render_template('suites.html', suitestr="ERROR ERROR ERROR")
     return render_template('suites.html', suitestr=':'.join(optvals))
 
